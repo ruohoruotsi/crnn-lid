ff169b1861f6ab7816c2f4d0198900a08df1db62
Iroro Orife <iorife@netflix.com>
2019-04-24 09:17:32 -0700

    [ADD] mods to runner

--- run.sh
+++ run.sh
@@ -4,7 +4,9 @@
 # ls *.m4a | xargs -P 5 -I % ffmpeg -ss 0 -to 3000  -i % -ac 1 -ar 16000 %.wav
 # courtesy of https://www.joshcurry.co.uk/posts/ffmpeg-convert-multiple-files-using-xargs
 
+# run this script like so: $ find  /Users/iorife/Desktop/testaudio/speech -type f -name "*.wav" | xargs -P 5 -I % ./run.sh %
+
 source venv/bin/activate
-cd /Users/iroro/github/crnn-lid/keras
-python predict.py --model ../web-server/model/2017-01-31-14-29-14.CRNN_EN_DE_FR_ES_CN_RU.model --input $1 > $1.results.txt 
+cd /Users/iorife/github/crnn-lid/keras
+python predict.py --model ../web-server/model/2017-01-31-14-29-14.CRNN_EN_DE_FR_ES_CN_RU.model --input $1 > $1".results.txt"
 deactivate 
